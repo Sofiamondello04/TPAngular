709d3c2624b76dab210d809aa88215b80b87db21
Sofia Mondello <sofiarmondello@gmail.com>
2022-11-28 19:18:45 -0300

    agrego directiva ngClass y campo clearance

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -14,21 +14,24 @@ export class ProductListComponent implements OnInit {
       name: 'Baba mom',
       type: 'Babero',
       price: 200,
-      stock: 15,
+      stock: 0,
+      clearance: false,
       image: 'assets/img/Baberos.png',
     },
     {
       name: 'Recibiendo',
       type: 'Ajuar',
       price: 790,
-      stock: 10,
+      stock: 0,
+      clearance: true, 
       image: 'assets/img/Ajuar.png',
     },
     {
       name: 'Glamm',
       type: 'Body',
       price: 630,
-      stock: 14,
+      stock: 0, 
+      clearance: true,
       image: 'assets/img/Body.png',
     }];
 
