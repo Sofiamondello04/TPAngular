fd7eaf3329971cc019dc36b434f965d0605ecce6
Sofia Mondello <sofiarmondello@gmail.com>
2022-11-29 18:08:37 -0300

    agrego funciones upQuantity y downQuantity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { FormsModule } from '@angular/forms';
+
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Product} from './Product';
 
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -10,34 +11,55 @@ export class ProductListComponent implements OnInit {
 
 
    products: Product [] = [
-    {
+    { 
+      image: 'assets/img/Baberos.png',
       name: 'Baba mom',
       type: 'Babero',
       price: 200,
-      stock: 0,
+      stock: 10,
       clearance: false,
-      image: 'assets/img/Baberos.png',
+      quantity: 0,
+      
     },
     {
+      image: 'assets/img/Ajuar.png',
       name: 'Recibiendo',
       type: 'Ajuar',
       price: 790,
       stock: 0,
       clearance: true, 
-      image: 'assets/img/Ajuar.png',
+      quantity: 0,
+      
     },
     {
+      image: 'assets/img/Body.png',
       name: 'Glamm',
       type: 'Body',
       price: 630,
       stock: 0, 
       clearance: true,
-      image: 'assets/img/Body.png',
+      quantity: 0,
+      
     }];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  upQuantity(product: Product): void {
+    if(product.quantity< product.stock) {
+    product.quantity++;
+    }
+  }
+  downQuantity(product: Product): void {
+    if(product.quantity>0) {
+    product.quantity--;
+    }
+  }
+  changeQuantity(event: { key: any; }, product: Product): void {
+    console.log(event.key);
+  }
+
 
 }
