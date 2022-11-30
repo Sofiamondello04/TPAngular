20afeee64aab8fa2e203e905b9035fb1e28c430a
Sofia Mondello <sofiarmondello@gmail.com>
2022-11-30 19:33:50 -0300

    agrego funcion getAll

--- src/app/product-data.service.ts
+++ src/app/product-data.service.ts
@@ -1,15 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Product } from './product-list/Product';
+
+const URL= 'https://6387cbe2d94a7e50408db836.mockapi.io/api/products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductDataService {
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient) { }
     
+    public getAll(): Observable<Product[]> {
+      return this.http.get<Product[]>(URL);
+      //retorna un observable del arreglo JSON
+    }
 
 
-
-  }
+  
 }
